63593ab5f3ff24367f1721e63c8ca92ac5a0b705
Dave Spina <dspina79@gmail.com>
2022-01-31 20:58:02 -0500

    Added notes on variable basics

--- HelloKotlin2/src/main/kotlin/Main.kt
+++ HelloKotlin2/src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
+// mutable global variable
+var name = "Unknown"
+// immutable member
+val pi = 3.14159
+
 fun main(args: Array<String>) {
     println("Hello, Kotlin")
+
+    println(pi)
+    println(name)
+
+    name = "Dean"
+    println(name)
+
 }
