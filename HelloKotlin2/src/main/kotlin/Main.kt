18625e0e5842c5ee7aa25b3d7a5484d65818dfdf
Dave Spina <dspina79@gmail.com>
2022-02-01 20:17:51 -0500

    Added conditionals and when statement

--- HelloKotlin2/src/main/kotlin/Main.kt
+++ HelloKotlin2/src/main/kotlin/Main.kt
@@ -1,15 +1,29 @@
 // mutable global variable
-var name = "Unknown"
+var name: String? = "Unknown" // string is nullable
 // immutable member
-val pi = 3.14159
+val pi: Double = 3.14159
 
 fun main(args: Array<String>) {
     println("Hello, Kotlin")
 
-    println(pi)
-    println(name)
+    println(pi) // prints 3.14159
+    println(name) // prints Unknown
 
     name = "Dean"
-    println(name)
+    println(name) // prints Dean
+    name = null
+    if (name != null) {
+        println(name)
+    } else {
+        println("Name is null")
+    }
 
+    // when statements, the equivalent of switch/case
+    var jobRole: String? = null
+    when (jobRole) {
+        null -> println("No role defined")
+        "Manager" -> println("What type of manager?")
+        "Executive" -> println("Big Boss")
+        else -> "something else"
+    }
 }
