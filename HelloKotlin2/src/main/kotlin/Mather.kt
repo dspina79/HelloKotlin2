5e61c0a5d722eb4ad16a93a5c7a79a3a34fe515f
Dave Spina <dspina79@gmail.com>
2022-02-14 20:20:33 -0500

    Added advanced mather

--- HelloKotlin2/src/main/kotlin/Mather.kt
+++ HelloKotlin2/src/main/kotlin/Mather.kt
@@ -9,3 +9,36 @@ class Mather {
         }
     }
 }
+
+class AdvancedMather {
+    companion object {
+        fun square(x:Int):Int {
+            return x * x
+        }
+
+        fun exp(base:Int, power:Int): Int {
+            var starter = base
+            var p = power
+            while (p > 1) {
+                starter *= base
+                p -= 1
+            }
+
+            if (power == 0) {
+                starter = 1
+            }
+
+            return starter
+        }
+    }
+}
+
+fun main() {
+    val x = 10
+    val x2 = AdvancedMather.square(x)
+    val x5 = AdvancedMather.exp(x, 5)
+
+    println("x is $x")
+    println("x squared is $x2")
+    println("x to the 5th power is $x5")
+}
